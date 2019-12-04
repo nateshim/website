6981cc9d55c2ceff14a909f9c34a668e60475fd2
Nathanael Shim <nateshim@berkeley.edu>
2019-12-04 15:17:55 -0800

    added music and projects pages

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//using strict mode reduces silent errors, improves performance, provides warnings, and fewer unsafe features
+ //using strict mode reduces silent errors, improves performance, provides warnings, and fewer unsafe features
 'use strict';
 console.log("testing console\'s log");
 
@@ -6,5 +6,6 @@ let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+document.body.style.backgroundImage = "url('http://getwallpapers.com/wallpaper/full/a/f/a/693660-good-background-picture-1920x1080-for-mobile-hd.jpg')";
 
 
