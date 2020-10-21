30bb2bee273436d6d3ab9281bf27ef16fdd38314
Nathanael Shim <nateshim@berkeley.edu>
2020-10-21 12:14:56 -0700

    Updates

---
+++ precache-manifest.66af968fe00010fb9dc1c4b2cc3b3f75.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0124a270e978edeb04b3ea17f165948",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "5c16e52c6717fb87fd27",
+    "url": "/website/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "cd251545c66eedcc0e61",
+    "url": "/website/static/css/main.46860370.chunk.css"
+  },
+  {
+    "revision": "5c16e52c6717fb87fd27",
+    "url": "/website/static/js/2.dded5b1d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/website/static/js/2.dded5b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd251545c66eedcc0e61",
+    "url": "/website/static/js/main.90c602ce.chunk.js"
+  },
+  {
+    "revision": "6246a29268a1e9378cdd",
+    "url": "/website/static/js/runtime-main.26a26a2d.js"
+  },
+  {
+    "revision": "070875f6a16dddf088c36295ae35beff",
+    "url": "/website/static/media/CS1.070875f6.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c29786d6b49230a2418ad65b75286c7b",
+    "url": "/website/static/media/javascript.c29786d6.png"
+  },
+  {
+    "revision": "2205ffb46e322c151d438108cc33db18",
+    "url": "/website/static/media/nateshim.2205ffb4.png"
+  },
+  {
+    "revision": "897410c88a097d1d617b4c1c774ba109",
+    "url": "/website/static/media/newebcb.897410c8.png"
+  },
+  {
+    "revision": "52e8bc6e1b9bc3c7992aadb314703526",
+    "url": "/website/static/media/node.52e8bc6e.png"
+  },
+  {
+    "revision": "e5a2165117ba2991cf0563f4370f2e9a",
+    "url": "/website/static/media/postnobillscolombo-regular.e5a21651.ttf"
+  },
+  {
+    "revision": "d9c6e4b28b585b99cc1c2233ebc246ff",
+    "url": "/website/static/media/python.d9c6e4b2.png"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/website/static/media/react.82a318cc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8939971b2c8a6c0bf7d13cd0553faa94.js"
+  "/website/precache-manifest.66af968fe00010fb9dc1c4b2cc3b3f75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
