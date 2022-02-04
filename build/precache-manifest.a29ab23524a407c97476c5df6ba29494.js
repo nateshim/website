d058152f686345d6b71fd5ab4f9cd19a2c2e975d
Nathanael Shim <nathanaelshim@Nathanaels-Mac-mini.local>
2022-02-03 17:00:20 -0800

    adding build

---
+++ build/precache-manifest.a29ab23524a407c97476c5df6ba29494.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c312232d65f18fc29a816bd8130d3da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d17a78c13fd665215caf",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "78a802c5a84911d3128d",
+    "url": "/static/css/main.60e6715b.chunk.css"
+  },
+  {
+    "revision": "d17a78c13fd665215caf",
+    "url": "/static/js/2.3777420f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3777420f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a802c5a84911d3128d",
+    "url": "/static/js/main.3134dd9c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4a85586095354992ca66b4cf816c00b3",
+    "url": "/static/media/gamefeed.4a855860.png"
+  },
+  {
+    "revision": "e5a2165117ba2991cf0563f4370f2e9a",
+    "url": "/static/media/postnobillscolombo-regular.e5a21651.ttf"
+  },
+  {
+    "revision": "34bd5b5a2f8c76c5c37e7d2b4c25a30a",
+    "url": "/static/media/puzzlechess.34bd5b5a.png"
+  }
+]);
